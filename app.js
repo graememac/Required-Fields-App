ffa18fc48804da252b6f34d93a4da2ad602f96be
Graeme <gsmac_@hotmail.com>
2015-06-09 22:29:13 +0100

    app now dynamically checks for required fields

--- app.js
+++ app.js
@@ -2,10 +2,22 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.activated':'getRquiredFields'
     },
-
-    doSomething: function() {
+    
+    getRquiredFields: function(){
+        
+        var required_fields_array = [];
+        var ticket_fields = this.ticketFields();
+        
+        // add field's name to required_fields_array if isRequired() returns true
+        for (var i=0; i<ticket_fields.length; i++){
+            if (ticket_fields[i].isRequired() === true){
+                required_fields_array.push(ticket_fields[i].name());
+            }
+        }
+        
+        this.switchTo('required_fields', { 'required_fields':required_fields_array });
     }
   };
 
